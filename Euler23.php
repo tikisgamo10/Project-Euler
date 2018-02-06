7ebc268cc504d8cea6aece6912b9f55f04c5e2b5
Tikis <ljgamo10@gmail.com>
2018-02-05 23:25:41 -0600

    Remove debug comments from sol'n 23

--- Euler23.php
+++ Euler23.php
@@ -15,7 +15,6 @@
 		return $properDivisors;
 	}
 
-	echo "Program has started\n";
 	$abundant_numbers = array();
 
 	for($i = 2; $i < 28124; $i++){
@@ -24,7 +23,6 @@
 		}
 	}
 
-	echo "Finished getting abundant_numbers\n";
 
 	for($i = 1; $i < 28124; $i++){
 		$sieve[$i] = false;
@@ -35,9 +33,6 @@
 	for($i = 0; $i < $num_of_abundant_numbers; $i++){
 		for($j = $i; $j < $num_of_abundant_numbers; $j++){
 			$sieve[$abundant_numbers[$i] + $abundant_numbers[$j]] = true;
-		}
-		if($i % 100 === 0){
-			"Finished {$i} outer loops on sieving\n";
 		}
 	}
 
